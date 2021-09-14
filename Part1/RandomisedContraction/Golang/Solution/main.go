30468840720d7e970f7f21aff645dc0e35d0f6e9
firgun <keaganroosdev@gmail.com>
2021-09-14 18:31:07 +0200

    Add more test cases for graph loading

--- Part1/RandomisedContraction/Golang/Solution/main.go
+++ Part1/RandomisedContraction/Golang/Solution/main.go
@@ -18,6 +18,13 @@ type edge struct {
 	a, b int
 }
 
+/*
+type graph struct {
+	nodes map[int]node
+	edges map[int]edge
+}
+*/
+
 type graph struct {
 	nodes []node
 	edges []edge
@@ -32,13 +39,13 @@ func (g *graph) String() string {
 				continue
 			}
 			incident := false
-			for _, ae := range a.edges {	
+			for _, ae := range a.edges {
 				edge := g.edges[ae]
 				if edge.a == b.id || edge.b == b.id {
 					incident = true
 					break
 				}
-			}	
+			}
 			if incident {
 				buf.WriteString("% ")
 			} else {
@@ -63,8 +70,9 @@ func load(path string) (*graph, error) {
 			return nil, fmt.Errorf("error occurred while scanning: %v", err)
 		}
 		ss := strings.Split(s.Text(), " ")
-		nd := node{len(g.nodes)+1, make([]int, 0, len(ss))}
-		for i, s := range ss {
+		nd := node{len(g.nodes), make([]int, 0, len(ss))}
+		g.nodes = append(g.nodes, nd)
+		for _, s := range ss {
 			if s == "" {
 				continue
 			}
@@ -72,18 +80,27 @@ func load(path string) (*graph, error) {
 			if err != nil {
 				return nil, fmt.Errorf("invalid token: %v", err)
 			}
-			e := edge{i, n}
-			g.edges = append(g.edges, e)
-			nd.edges = append(nd.edges, len(g.edges)-1)
+			g.edges = append(g.edges, edge{nd.id, n - 1})
 		}
-		g.nodes = append(g.nodes, nd)
+	}
+	for i, ed := range g.edges {
+		na := &g.nodes[ed.a]
+		nb := &g.nodes[ed.b]
+		na.edges = append(na.edges, i)
+		nb.edges = append(nb.edges, i)
 	}
 	return g, nil
 }
 
-func main() {
-	tests := [...]string{"complete", "islands"}
-
+func testGraphLoad() {
+	tests := [...]string{
+		"complete",
+		"islands",
+		"star",
+		"cycle",
+		"binary_tree_breadth_first",
+		"binary_tree_depth_first",
+	}
 	for _, t := range tests {
 		g, err := load(fmt.Sprintf("../../Tests/%s.input", t))
 		if err != nil {
@@ -92,4 +109,25 @@ func main() {
 		}
 		fmt.Println(g)
 	}
+}
+
+// contract fuses the two endpoints of a specified edge together creating a
+// supernode. The resulting supernode is adjacent to all nodes either of the
+// endpoints where adjacent to.
+func contract(g *graph, edgeIdx int) {
+	/*
+		e := &g.edges[edgeIdx]
+		for _, bEdgeIdx := range e.b {
+			bEdge := g.edges[bEdgeIdx]
+			if bEdge.a == e.b {
+				bEdge.a = e.a
+			} else {
+				bEdge.b = e.a
+			}
+		}
+	*/
+}
+
+func main() {
+
 }
