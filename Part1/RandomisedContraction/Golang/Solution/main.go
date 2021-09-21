488d8174255ac6a7a62039e6fe9b92d508de887e
firgun <keaganroosdev@gmail.com>
2021-09-21 13:01:25 +0200

    Implement random contraction trial -- with bugs

--- Part1/RandomisedContraction/Golang/Solution/main.go
+++ Part1/RandomisedContraction/Golang/Solution/main.go
@@ -1,38 +1,46 @@
 package main
 
+//
 // todo
-// ---
-// for each endpoint of an edge, store a pointer to a pointer to a node
 //
-// for each node, store an array of array of edges
+// If we per chance pick an edge that happens to be a self loop, then we get
+// rid of it and never have to deal with it again.
 //
-// a supernode is just a node with the top-level array having more than one
-// element
+// What's an upper bound on the number of self-loops that can form throughout
+// the algorithm's execution?
 //
-// if you want to contract a supernode and a node, you just add the one element
-// in the node to the array in the supernode O(1)
+// If the number of self-loops we have to deal with is linear over the whole
+// execution of the algorithm then we only do linear work in addition to our
+// already linear work and the running time of a single trial stays linear.
 //
-// if you want to contract two regular ndoes, just pretend that the one with
-// more incident edges is the supernode, and do as above O(1)
+// there obviously can't be more than m self-loops, because all self-loops must
+// come from an edge in the initial state of the graph.
 //
-// if you want to contract two supernodes together, just concatenate the
-// smaller to the larger, so the larger supernode becomes the new supernode
-// O(n) time
+// the number of self-loops is actually m - k? Isn't it? Because all edges that
+// don't end up as self-loops at the end of the execution end up as crossing
+// edges.
 //
-// make a shuffled array of edges up front, then for each contraction pick pop
-// an edge of the array.
+// Well, that's linear in the number of edges, and we have to read each edge
+// while reading the input.
 //
-// the assignments format specifies a single edge "from the point of view" of
-// both endpoints
+// So it wouldn't take more time than reading the input -- asymptotically
+// speaking.
 //
-// handle parallel edges in graph loading
+// so we don't need to delete a self-loop until we randomly sample it.
+//
+// we can have a shuffled array of m edges, then pick at random, if the edge
+// picked at random is a self-loop, then we discard it and try again. We'll end
+// up discarding at most m - k edges over the whole execution which is
+// obviously only linear in the number of edges.
 
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Graph struct {
@@ -46,10 +54,6 @@ type Node struct {
 }
 
 type Edge [2]int
-
-func (e Edge) String() string {
-	return fmt.Sprintf("{%d, %d}", e[0]+1, e[1]+1)
-}
 
 func NewEdge(a, b int) Edge {
 	e := Edge([2]int{a, b})
@@ -131,26 +135,35 @@ func (g *Graph) dump() {
 }
 
 func (g *Graph) contract(e *Edge) {
-	// fmt.Println(e)
+	fmt.Printf("contracting %d <- %d\n", g.Nodes[e[0]].Label+1, g.Nodes[e[1]].Label+1)
 	for _, edgeList := range g.Nodes[e[1]].Edges {
 		g.Nodes[e[0]].Edges = append(g.Nodes[e[0]].Edges, edgeList)
 	}
 	g.Nodes[e[1]].Edges = nil
 	g.Nodes[e[1]] = g.Nodes[e[0]]
+
+	g.dump()
+	fmt.Println()
 }
 
 func (g *Graph) minCut() int {
-	// *old_ptr_ptr = *last_ptr_ptr
-	// pop(node_array)
-
-	/*
-		for nodesLeft := len(g.Nodes); nodesLeft > 2; nodesLeft-- {
-
+	ids := make([]int, len(g.Edges))
+	for i, _ := range ids {
+		ids[i] = i
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(ids), func(i, j int) {
+		ids[i], ids[j] = ids[j], ids[i]
+	})
+	next := 0
+	edgeIndex := ids[next]
+	for nodesLeft := len(g.Nodes); nodesLeft > 2; nodesLeft-- {
+		for g.Nodes[g.Edges[edgeIndex][0]].Label == g.Nodes[g.Edges[edgeIndex][1]].Label {
+			next++
+			edgeIndex = ids[next]
 		}
-	*/
-
-	g.contract(g.Edges[1])
-
+		g.contract(g.Edges[edgeIndex])
+	}
 	return -1000
 }
 
@@ -159,15 +172,11 @@ func main() {
 		fmt.Println("error: bad args, specify path to graph input file")
 		os.Exit(1)
 	}
-
 	g, err := readGraph(os.Args[1])
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		os.Exit(1)
 	}
-
-	g.dump()
-	g.contract(g.Edges[0])
-	fmt.Println()
-	g.dump()
+	fmt.Println(g.minCut())
+	fmt.Println(g)
 }
