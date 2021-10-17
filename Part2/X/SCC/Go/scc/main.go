c3a08fe1a431a208a60877352767171a90b2cd31
firgun <keaganroosdev@gmail.com>
2021-10-17 11:14:13 +0200

    Implement automatic testing of generated test cases

--- Part2/X/SCC/Go/scc/main.go
+++ Part2/X/SCC/Go/scc/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"sort"
@@ -25,7 +26,7 @@ func loadGraph(p string) (*graph, error) {
 	defer f.Close()
 	s := bufio.NewScanner(f)
 	g := graph{}
-	prev := -1
+	prev := 0
 	for s.Scan() {
 		parts := strings.Split(s.Text(), " ")
 		hs, ts := parts[0], parts[1]
@@ -37,19 +38,21 @@ func loadGraph(p string) (*graph, error) {
 		if err != nil {
 			return nil, err
 		}
-		if prev == -1 || prev != h {
-			g = append(g, node{})
+		if prev == 0 || prev != h {
+			for i := prev + 1; i <= h; i++ {
+				g = append(g, node{})
+			}
 			prev = h
 		}
 		g[len(g)-1].o = append(g[len(g)-1].o, t-1)
+	}
+	if s.Err() != nil {
+		return nil, s.Err()
 	}
 	for ni, n := range g {
 		for _, a := range n.o {
 			g[a].i = append(g[a].i, ni)
 		}
-	}
-	if s.Err() != nil {
-		return nil, s.Err()
 	}
 	return &g, nil
 }
@@ -62,9 +65,6 @@ func dfs1(g graph, n int, v []bool, f []int, t *int) {
 		}
 	}
 	*t++
-	if f[n] != 0 {
-		panic("assigning finishing time to a node twice!!!")
-	}
 	f[n] = *t
 }
 
@@ -129,13 +129,118 @@ func sccs(g graph) []int {
 	return r
 }
 
+func loadSlice(path string) ([]int, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	toks := strings.Split(string(b), ",")
+	r := make([]int, 0, 5)
+	for _, t := range toks {
+		n, err := strconv.Atoi(strings.Trim(t, " \n\t\r"))
+		if err != nil {
+			return nil, err
+		}
+		r = append(r, n)
+	}
+	return r, nil
+}
+
+func runGeneratedTestCases() {
+	resourcesPath, ok := os.LookupEnv("ALGO_PATH")
+	if !ok {
+		fmt.Println("error: cannot locate resource directory, exiting ...")
+		os.Exit(1)
+	}
+	basePath := path.Join(resourcesPath, "Tests/X/Generated")
+	fInfos, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Printf("error: cannot read test directory: %v\n", err)
+		os.Exit(1)
+	}
+	testCases := make([]struct {
+		In  string
+		Out string
+	}, len(fInfos)/2)
+	for i, info := range fInfos {
+		if info.Mode().IsRegular() && strings.HasPrefix(info.Name(), "input") {
+			testCases[i].In = info.Name()
+			testCases[i].Out = strings.Replace(info.Name(), "input", "output", 1)
+		}
+	}
+	failCount := 0
+	for _, tc := range testCases {
+		g, err := loadGraph(path.Join(basePath, tc.In))
+		if err != nil {
+			fmt.Printf("failed to load graph: %v\n", err)
+			os.Exit(1)
+		}
+		expected, err := loadSlice(path.Join(basePath, tc.Out))
+		if err != nil {
+			fmt.Printf("failed to load output: %v\n", err)
+			os.Exit(1)
+		}
+		actual := sccs(*g)
+		fmt.Print("expected: ")
+		for i, n := range expected {
+			fmt.Print(n)
+			if i != len(expected)-1 {
+				fmt.Print(",")
+			}
+		}
+		fmt.Print("  actual: ")
+		for i, n := range actual {
+			fmt.Print(n)
+			if i != len(actual)-1 {
+				fmt.Print(",")
+			}
+		}
+		fail := false
+		if len(expected) != len(actual) {
+			fail = true
+		}
+		if !fail {
+			for idx := range expected {
+				if expected[idx] != actual[idx] {
+					fail = true
+					break
+				}
+			}
+		}
+		if fail {
+			fmt.Println("  FAIL")
+			failCount++
+		} else {
+			fmt.Println("  PASS")
+		}
+	}
+	fmt.Println("===\nTOTAL TEST CASES FAILED:", failCount, "\n")
+
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("usage: scc <input>.txt")
 		os.Exit(1)
 	}
-	fileName := os.Args[1]
-	resourcesPath, ok := os.LookupEnv("ALGO_RESOURCE_DIRECTORY")
+	arg := os.Args[1]
+
+	if strings.HasPrefix(arg, "-") {
+		if arg == "-g" || arg == "--generated" {
+			runGeneratedTestCases()
+		} else {
+			fmt.Printf("error: unknown arg %v\n", arg)
+		}
+		return
+	}
+	fileName := arg
+
+	resourcesPath, ok := os.LookupEnv("ALGO_PATH")
 	if !ok {
 		fmt.Println("error: cannot locate resource directory, exiting ...")
 		os.Exit(1)
